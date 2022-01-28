23274c74ad3f4d85a26872ef60ab6850265e1be6
Lalit Ghimire <lalitghimire@gmail.com>
2022-01-28 13:52:13 +0200

    add routes

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
-import express from "express";
+const express = require("express");
 const app = express();
 
+app.use(express.json());
+const users = [];
+
+// route for all users
 app.get("/users", (req, res) => {
-  res.json("Hello");
+  res.json(users);
+});
+
+//route for posting a user
+app.post("/users", (req, res) => {
+  const user = { name: req.body.name, password: req.body.password };
+  console.log(user);
+  users.push(user);
+  res.status(201).send();
 });
 
 app.listen(3000);
